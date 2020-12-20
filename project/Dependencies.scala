cd8cd34b679dd912f90e4be1a0d8f1aaf099d195
Fernando Brûlé <fernandobrule@gmail.com>
2020-12-20 16:26:35 -0300

    update dependecies version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,12 +6,12 @@ object Dependencies {
   lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3" % "runtime"
   lazy val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.25"
   lazy val scalatest = "org.scalatest" %% "scalatest" % "3.0.8" % "test"
-  lazy val mysql8JavaClient = "mysql" % "mysql-connector-java" % "8.0.18"
+  lazy val mysql8JavaClient = "mysql" % "mysql-connector-java" % "8.0.22"
 
   //
   // fragnostic
   //
-  lazy val fragnosticDaoApi = "com.fragnostic" % "fragnostic-dao-api_2.13" % "0.1.23"
-  lazy val fragnosticSupport = "com.fragnostic" % "fragnostic-support_2.13" % "0.1.14"
+  lazy val fragnosticDaoApi = "com.fragnostic" % "fragnostic-dao-api_2.13" % "0.1.24-SNAPSHOT"
+  lazy val fragnosticSupport = "com.fragnostic" % "fragnostic-support_2.13" % "0.1.15-SNAPSHOT"
 
 }
