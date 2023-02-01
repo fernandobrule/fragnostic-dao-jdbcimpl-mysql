dd7bed54cd6113c5326d6b5f03979464ae56170c
Fernando Brûlé <fernandobrule@gmail.com>
2023-02-01 11:11:04 -0300

    find list fix

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,6 +9,6 @@ object Dependencies {
   lazy val mysql8JavaClient           = "mysql"                 % "mysql-connector-java"           % "8.0.27"
   lazy val logbackClassic             = "ch.qos.logback"        % "logback-classic"                % "1.3.0-alpha12" % "runtime"
   lazy val slf4jApi                   = "org.slf4j"             % "slf4j-api"                      % "2.0.0-alpha5"
-  lazy val scalatestFunSpec           = "org.scalatest"        %% "scalatest-funspec"              % "3.3.0-SNAP3" % Test
+  lazy val scalatestFunSpec           = "org.scalatest"         % "scalatest-funspec_2.13"         % "3.3.0-SNAP3" % Test
 
 }
